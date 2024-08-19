0889e5e5780f2500720f5b196d1498d50da89f82
Linh Pham <question+github@closedsrc.org>
2024-08-19 08:48:22 -0700

    Cleanup code, update error templates, add several new CSS generators

---
+++ runner-remote.sh
@@ -0,0 +1,7 @@
+#!/bin/sh
+# Shell script used to start up Flask for local development and testing
+export FLASK_APP=app
+export FLASK_DEBUG=1
+export FLASK_RUN_PORT=8000
+. venv/bin/activate
+flask run --host=0.0.0.0
