32c57ec64917c13fecc503f87f819f2dc0ff0cf1
Linh Pham <question+github@closedsrc.org>
2024-08-20 21:42:13 -0700

    Adding tooltips, fixing spacing issues between blocks in details pages

--- app/static/js/app-code.js
+++ app/static/js/app-code.js
@@ -79,7 +79,6 @@
     })
 })()
 
-
 /*!
  * stats.wwdt.me (https://github.com/questionlp/stats.wwdt.me)
  * Copyright 2018-2024 Linh Pham, Elijah Conners
@@ -105,3 +104,15 @@ if (document.getElementById("map")) {
     marker.bindPopup(tooltip);
 
 }
+
+/* Bootstrap Popovers */
+var popoverTriggerList = [].slice.call(document.querySelectorAll('[data-bs-toggle="popover"]'));
+var popoverList = popoverTriggerList.map(function (popoverTriggerEl) {
+    return new bootstrap.Popover(popoverTriggerEl)
+});
+
+/* Bootstrap Tooltips */
+var tooltipTriggerList = [].slice.call(document.querySelectorAll('[data-bs-toggle="tooltip"]'));
+var tooltipList = tooltipTriggerList.map(function (tooltipTriggerEl) {
+    return new bootstrap.Tooltip(tooltipTriggerEl)
+});
